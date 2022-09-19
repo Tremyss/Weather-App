276d628cef744d490573bd3e1d266e708f82d35a
Tremyss <tremyss1@gmail.com>
2022-09-19 15:56:08 +0200

    search for cities functions

--- js/script.js
+++ js/script.js
@@ -1,14 +1,41 @@
 var root = document.querySelector("#root");
-Lon
-const lekeres = async () =>  {
-    var city = kereso.value;
-    var fetchPromise = await fetch (`http://api.weatherapi.com/v1/current.json?key=e6be19e096224376bf9100012221909&q=${city + *}`) 
+var searchBar = document.querySelector("#searchbar");
+var cityName = ""
 
-   
+// todo 2 function, 1 fetch (nagy városlista), 1 other
+
+const cityFinder = async () => {
+    // ? a cityname mindig a keresőmező értékével lesz egyenlő, majd + fg-ben karakterletésenként frissítsük az értékét, ez lehet keypress után
+    var fetchCity = await fetch ('https://countriesnow.space/api/v0.1/countries')
+    var fetchCityContent = await fetchCity.json();
+    /* console.log(fetchCityContent.data[0].cities[1]) */
+    // ? 2 for loop, 1 a datán megy, 1 pedig az adott országhoz tartozó város i-edik tagján
+    for (let i=0; i<fetchCityContent.data.length; i++) {
+        for (let j=0; j<fetchCityContent.data[i].cities.length; j++) {
+            if (fetchCityContent.data[i].cities[j].startsWith(cityName)) {
+                console.log(fetchCityContent.data[i].cities[j], fetchCityContent.data[i].country);
+            }            
+        }
+    }
+    
+    /* console.log(fetchCityContent.data[0].cities[0]);
+    // ez a fenti kód hozza elő az adott (i) ország adott (j) városát
+
+    console.log(fetchCityContent);
+    // ez meg a full objektumot */
 }
 
 
 
-inputmezo.addEventListener("keypress", lekeres)
+// ? függvény, ami az input mező value-ját összehasonlítjuk minden ország minden városnevével
 
-"Lo            "
+const searchCity = () => {
+    console.clear();
+    if (searchBar.value.length>=3) {
+        console.log(".............ahoy.........");
+        cityName = searchBar.value;
+        cityFinder();
+    }
+}
+
+searchBar.addEventListener("input", searchCity);
