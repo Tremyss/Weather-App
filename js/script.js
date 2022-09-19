e94ebb285a921fd12b6346089f03be7b0d7d119c
Richard Szabo <foldimogyoro2000@gmail.com>
2022-09-19 13:05:06 +0200

    Base files.

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var root = document.querySelector("#root");
+Lon
+const lekeres = async () =>  {
+    var city = kereso.value;
+    var fetchPromise = await fetch (`http://api.weatherapi.com/v1/current.json?key=e6be19e096224376bf9100012221909&q=${city + *}`) 
+
+   
+}
+
+
+
+inputmezo.addEventListener("keypress", lekeres)
+
+"Lo            "
