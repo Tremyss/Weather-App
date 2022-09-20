449d7ad0469b5dc6649647856c862d486b78c74c
foldimogyoro2000@gmail.com <foldimogyoro2000@gmail.com>
2022-09-20 20:01:02 +0200

    Day 02 save - Search algorithm is almost done, weather API fetch is working.

--- js/script.js
+++ js/script.js
@@ -5,8 +5,17 @@ var dropdown = document.querySelector('.dropdown');
 var dropdownMatch = document.querySelector('.match');
 var countryMatchUnderscored = ""
 var createDivValue = ""
+var foundLocations = [];
 
 // todo 2 function, 1 fetch (nagy városlista), 1 other
+
+var showInfos = async (event) => {
+    var fetchWeather = await fetch(`https://api.weatherapi.com/v1/current.json?key=e6be19e096224376bf9100012221909&q=${foundLocations[event.target.getAttribute("value")].city}+${foundLocations[event.target.getAttribute("value")].country}`)
+    var fetchWeatherContent = await fetchWeather.json();
+    console.log(fetchWeatherContent);
+    searchBar.value = foundLocations[event.target.getAttribute("value")].city + ", " + foundLocations[event.target.getAttribute("value")].country;
+}
+
 
 const cityFinder = async () => {
     // ? a cityname mindig a keresőmező értékével lesz egyenlő, majd + fg-ben karakterletésenként frissítsük az értékét, ez lehet keypress után
@@ -14,56 +23,58 @@ const cityFinder = async () => {
     var fetchCityContent = await fetchCity.json();
     // console.log(fetchCityContent.data)
     // ? 2 for loop, 1 a datán megy, 1 pedig az adott országhoz tartozó város i-edik tagján
-    for (let i=0; i<fetchCityContent.data.length; i++) {
-        for (let j=0; j<fetchCityContent.data[i].cities.length; j++) {
-            if (fetchCityContent.data[i].cities[j].startsWith(cityName)) {
-                var cityMatch = fetchCityContent.data[i].cities[j];
-                var countryMatch = fetchCityContent.data[i].country;
-                var createDiv = document.createElement("div");
-                countryMatchUnderscored = countryMatch.replace(/ /g, "_")
-                
-                console.log(countryMatchUnderscored)
 
-                
-                createDiv.classList.add("match");
-                createDiv.innerHTML = `${cityMatch}, ${countryMatch}`;
-                dropdown.appendChild(createDiv);
-                // console.log(cityMatch, countryMatch);
-                dropdown.style.display = "block";
-                // dropdown.style.flexDirection = "column"
-                // console.log(fetchCityContent.data[i].cities[j], fetchCityContent.data[i].country);
-                createDiv.addEventListener("click", () => {
-                    console.log(createDiv.textContent);
-                });
-                
-                
-
+        for (let i=0; i<fetchCityContent.data.length; i++) {
+            for (let j=0; j<fetchCityContent.data[i].cities.length; j++) {
+                if (fetchCityContent.data[i].cities[j].startsWith(cityName)) {
+    
+                    var cityMatch = fetchCityContent.data[i].cities[j];
+                    var countryMatch = fetchCityContent.data[i].country;
+                    foundLocations.push({city: `${cityMatch}`, country: `${countryMatch}` });
+    
+                }
             }
         }
-    }
 
-    
-    /* console.log(fetchCityContent.data[0].cities[0]);
-    // ez a fenti kód hozza elő az adott (i) ország adott (j) városát
-
-    console.log(fetchCityContent);
-    // ez meg a full objektumot */
+        return divGenerator();
 }
 
 
+const divGenerator = async () => {
+    for (let i = 0; i < foundLocations.length; i++) {
+        var divTemplate = document.createElement("div");
+        divTemplate.classList.add("match");
+        divTemplate.setAttribute("value", i);
+        divTemplate.innerHTML = `${foundLocations[i].city}, ${foundLocations[i].country}`;
+        divTemplate.addEventListener("click", showInfos);
+    dropdown.appendChild(divTemplate)
+    }
+} 
 
 // ? függvény, ami az input mező value-ját összehasonlítjuk minden ország minden városnevével
 
 const searchCity = () => {
     console.clear()
     dropdown.innerHTML = "";
+    foundLocations = [];
     if (searchBar.value.length>=3) {
+        dropdown.style.display = "flex";
+        dropdown.style.flexDirection = "column";
         cityName = searchBar.value.charAt(0).toUpperCase() + searchBar.value.slice(1, searchBar.value.length);
         cityFinder();
+    } else {
+        dropdown.style.display = "none";
     }
 }
 
 searchBar.addEventListener("input", searchCity);
+
+searchBar.addEventListener("keypress", (event) => {
+    if (event.key == "Enter") {
+        console.log("anyád");
+        showInfos();
+    }
+});
 
 
 
@@ -77,4 +88,6 @@ const currentWeather = async () => {
 }
 
 currentWeather()
+
+
 
